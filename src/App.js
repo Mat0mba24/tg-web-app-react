81412c706ef1a64611056ec897da8a18220e30d2
Mat0mba24 <mat0mba@mail.ru>
2024-04-29 14:43:59 +0300

    change imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import { Route, Routes } from 'react-router-dom';
-import SaleApartmentForm from "./components/SaleApartmentForm/SaleApartmentForm"
-import RentApartmentForm from "./components/RentApartmentForm/RentApartmentForm"
+import SaleApartmentForm from "./components/SaleApartmentForm/SaleApartmentForm";
+import RentApartmentForm from "./components/RentApartmentForm/RentApartmentForm";
 
 function App() {
 	const { tg } = useTelegram();
@@ -14,6 +14,7 @@ function App() {
 
 	return (
 		<div className="App">
+			work
 			<Routes>
 				<Route path={'sale_apartment_form'} element={<SaleApartmentForm />} />
 				<Route path={'rent_apartment_form'} element={<RentApartmentForm />} />
